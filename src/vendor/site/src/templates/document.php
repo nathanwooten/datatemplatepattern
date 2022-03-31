f16b52846c9593fcc74ca2b9fcd1c0d36c7d3424
nw <55707935+nathanwooten@users.noreply.github.com>
2022-03-31 15:36:26 -0400

    Create document.php

---
+++ src/vendor/site/src/templates/document.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>nathanwooten.net</title>
+<link rel="stylesheet" type="text/css" href="/static/styles/document.css">
+</head>
+<body>
+
+<?php print $content; ?>
