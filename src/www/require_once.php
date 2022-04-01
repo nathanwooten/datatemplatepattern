50c3b3408f8c5cdc82343edcd81dc91d44dd3c4c
nw <55707935+nathanwooten@users.noreply.github.com>
2022-04-01 18:08:53 -0400

    Add files via upload

---
+++ src/www/require_once.php
@@ -0,0 +1,36 @@
+<?php
+
+function dtaGetPath( $start, $finishFile )
+{
+
+	$msg = 'Unable to locate directory, %s';
+
+	if ( is_file( $start ) ) {
+		$dir = dirname( $start ) . DIRECTORY_SEPARATOR;
+	} else {
+		$dir = $start . DIRECTORY_SEPARATOR;
+	}
+
+	$file = $dir . $finishFile;
+
+	while ( ! is_readable( $file ) ) {
+
+		$before = $dir;
+
+		$dir = dirname( $dir );
+		$file = $dir . DIRECTORY_SEPARATOR . $finishFile;
+
+		if ( $dir === $before ) {
+			throw new Exception( sprintf( $msg, 'reached root' ) );
+		}
+	}
+
+	$dir .= DIRECTORY_SEPARATOR;
+
+	return $dir;
+
+}
+
+$path = dtaGetPath( __DIR__, 'project.txt' );
+
+require_once $path . 'require_once.php';
