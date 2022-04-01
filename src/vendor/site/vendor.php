1fae7169cd5f41ff21fcc5f2c88c47bca892abb1
nw <55707935+nathanwooten@users.noreply.github.com>
2022-04-01 18:07:12 -0400

    Add files via upload

---
+++ src/vendor/site/vendor.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once SITEPATH . 'functions.php';
+
+require_once SITEPATH . 'Registry' . DS . 'src' . DS . 'InputRegistryContainer.php';
+require_once SITEPATH . 'Registry' . DS . 'src' . DS . 'InputRegistry.php';
+require_once SITEPATH . 'Registry' . DS . 'src' . DS . 'OutputRegistry.php';
